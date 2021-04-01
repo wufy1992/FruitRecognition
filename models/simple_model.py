a3ae2c5f052f639ad21a80c1f1337fdf191dae81
wufy1992 <wufangyu1992@gmail.com>
2021-04-02 01:31:33 +0800

    【功能提交】完善simple模型的构建

--- models/simple_model.py
+++ models/simple_model.py
@@ -1,21 +1,31 @@
 #!/usr/bin/env python3.7
 # -*- coding: utf-8 -*-
-from keras.models import Sequential
-from keras.layers.core import Dense, Dropout, Activation, Flatten
-from keras.layers.convolutional import Convolution2D, MaxPooling2D
-from keras.optimizers import SGD
+from keras.models import Model
+from keras.layers import (
+        Input,
+        Flatten,
+        Dense,
+        ZeroPadding2D,
+        Conv2D,
+        Activation,
+        MaxPooling2D,
+        Dropout,
+        BatchNormalization)
+from keras.layers.advanced_activations import LeakyReLU
 
 
 def get_model(input_dim, category_num):
-    model = Sequential()
-    model.add(Convolution2D(4, (5, 5), activation = 'tanh',input_shape=(3, input_dim[0], input_dim[1]), data_format='channels_first'))
-    model.add(Convolution2D(8, (3, 3), activation = 'tanh', data_format='channels_first'))
-    model.add(MaxPooling2D(pool_size=(2, 2), data_format='channels_first'))
-    model.add(Convolution2D(16, (3, 3), activation = 'tanh', data_format='channels_first'))
-    model.add(MaxPooling2D(pool_size=(2, 2), data_format='channels_first'))
-    model.add(Flatten())
-    model.add(Dense(128))
-    model.add(Activation('tanh'))
-    model.add(Dense(category_num, init='normal'))
-    model.add(Activation('softmax'))
+    inputs = Input(shape=(input_dim[0], input_dim[1], 3))
+    x = ZeroPadding2D()(inputs)
+    x = Conv2D(8, (3, 3))(x)
+    x = Conv2D(16, (3, 3))(x)
+    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same')(x)
+    x = Conv2D(32, (3, 3))(x)
+    x = MaxPooling2D(pool_size=(3, 3), strides=(2, 2), padding='same')(x)
+    x = Flatten()(x)
+    x = Dense(256)(x)
+    x = Activation('tanh')(x)
+    x = Dense(category_num)(x)
+    x = Activation('softmax')(x)
+    model = Model(inputs=inputs, outputs=x)
     return model
