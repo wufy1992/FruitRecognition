541920d694d7416e7551f71b2eba92fe043ba072
wufy1992 <wufangyu1992@gmail.com>
2021-04-02 01:33:43 +0800

    【功能提交】完善水果图片获取，并添加模型训练结果

--- fruit_model.py
+++ fruit_model.py
@@ -2,13 +2,14 @@
 # -*- coding: utf-8 -*-
 import os
 import random
-import keras
 import cv2
+import xmltodict
 from keras.utils import np_utils
 import numpy as np
 from models import simple_model, ocr_model, vgg_model
-from keras.optimizers import SGD, Adadelta
+from keras.optimizers import Adadelta
 from keras.losses import categorical_crossentropy
+from keras.models import load_model
 
 
 class FruitModel(object):
@@ -18,9 +19,10 @@ class FruitModel(object):
         self.category_dict = {
             'apple': 0,
             'banana': 1,
-            'orange': 2,
-            'mixed': 3
+            'orange': 2
         }
+        # simple_model 500 epoch -- loss: 0.0949 - accuracy: 0.9925 - val_loss: 0.3382 - val_accuracy: 0.8866
+        # ocr_model    100 epoch -- loss: 0.0076 - accuracy: 1.0000 - val_loss: 0.1220 - val_accuracy: 0.9794
         self.model = ocr_model.get_model(input_dim, category_num)
 
     def train_and_save_model(self, train_data_dir, test_dir, model_path):
@@ -29,7 +31,7 @@ class FruitModel(object):
         label = np_utils.to_categorical(label, self.category_num)
         test_label = np_utils.to_categorical(test_label, self.category_num)
         self.model.compile(loss=categorical_crossentropy, optimizer=Adadelta(), metrics=['accuracy'])
-        self.model.fit(data, label, batch_size=128, epochs=50, verbose=1,  validation_data=(test_data, test_label))
+        self.model.fit(data, label, batch_size=32, epochs=300, verbose=1,  validation_data=(test_data, test_label))
         self.model.save(model_path)
 
     def load_data(self, data_dir):
@@ -39,11 +41,22 @@ class FruitModel(object):
             if file_name.endswith('.jpg'):
                 fruit_name = file_name.split('_')[0]
                 if fruit_name in self.category_dict:
-                    label_list.append(self.category_dict[fruit_name])
                     file_path = os.path.join(data_dir, file_name)
                     img = cv2.imread(file_path)
-                    img = cv2.resize(img, (self.input_dim[0], self.input_dim[1]))
-                    data_list.append(img)
+                    xml_path = os.path.join(data_dir, file_name.replace('jpg', 'xml'))
+                    if os.path.exists(xml_path):
+                        xml_str = open(xml_path, "r").read()
+                        xml_dict = xmltodict.parse(xml_str)
+                        objects = xml_dict['annotation']['object']
+                        if not isinstance(objects, list):
+                            objects = [objects]
+                        for object_node in objects:
+                            fruit_name = object_node['name']
+                            bndbox = object_node['bndbox']
+                            fruit_image = img[int(bndbox['ymin']):int(bndbox['ymax']), int(bndbox['xmin']):int(bndbox['xmax'])]
+                            fruit_image = cv2.resize(fruit_image, (self.input_dim[0], self.input_dim[1]))
+                            data_list.append(fruit_image)
+                            label_list.append(self.category_dict[fruit_name])
         data_np = np.array(data_list)
         label_np = np.array(label_list)
         index = [i for i in range(len(data_np))]
@@ -54,6 +67,9 @@ class FruitModel(object):
 
 
 if __name__ == "__main__":
+    model_path = 'models/ocr_model.h5'
     test_model = FruitModel([128, 128], 4)
-    test_model.train_and_save_model('train_data', 'test_data', 'models/test_model.h5')
+    if os.path.exists(model_path):
+        test_model.model = load_model(model_path)
+    test_model.train_and_save_model('train_data', 'test_data', model_path)
 
